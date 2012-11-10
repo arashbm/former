78310e4a07ce4c93feb995d731de85169998eaa2
Arash Badie Modiri <arashbm@gmail.com>
2012-11-10 11:04:26 +0330

    fixed sorting error in form model

--- app/models/form.rb
+++ app/models/form.rb
@@ -2,8 +2,8 @@ class Form < ActiveRecord::Base
   attr_accessible :description, :name, :field_ids, :enabled
 
   has_many :fields_forms 
-  has_many :fields, :through => :fields_forms, order: :position
-  has_many :filled_forms
+  has_many :fields, through: :fields_forms
+  has_many :filled_forms, order: :position
 
   validates :name, presence: true
 end
